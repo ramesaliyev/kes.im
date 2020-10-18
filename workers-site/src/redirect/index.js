23e279abfe09d51dc4efa799f9e7bc282668ee35
Rameş Aliyev <creator@ramesaliyev.com>
2020-10-18 21:00:19 +0300

    :sparkles: Use CF Cache

--- workers-site/src/redirect/index.js
+++ workers-site/src/redirect/index.js
@@ -5,12 +5,26 @@ export default event => {
 };
 
 async function redirect(event) {
-  const {pathname} = new URL(event.request.url);
+  const cache = caches.default
+  const {request} = event;
+
+  // Try to retrieve from cache.
+  const cached = await cache.match(request);
+  if (cached) return cached;
+
+  const {pathname} = new URL(request.url);
   const slug = pathname.substr(1);
   const url = await kesim_data.get(slug);
 
   if (url) {
-    return Response.redirect(url, 301);
+    const redirres = Response.redirect(url, 301);
+    const response = new Response(redirres.body, redirres);
+
+    // Cache
+    response.headers.append('Cache-Control', 'max-age=31536000');
+    event.waitUntil(cache.put(request, response.clone()));
+
+    return response;
   }
 
   return serve404(event);
