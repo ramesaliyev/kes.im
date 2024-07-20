580179e930db6ad28e0be94793ca9ae67a44225c
Rameş Aliyev <ramesaliyev@gmail.com>
2024-07-20 03:38:44 +0200

    use spaces for gods sake

--- workers-site/src/api/security.js
+++ workers-site/src/api/security.js
@@ -5,23 +5,23 @@ export async function validateToken(token, event) {
   const {headers} = event.request;
   const ip = headers.get('CF-Connecting-IP');
 
-	const result = await fetch(CF_TURNSTILE_VERIFY_URL, {
-		body: JSON.stringify({
-			secret: TURNSTILE_SECRET,
-			response: token,
-			remoteip: ip
-		}),
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
+  const result = await fetch(CF_TURNSTILE_VERIFY_URL, {
+    body: JSON.stringify({
+      secret: TURNSTILE_SECRET,
+      response: token,
+      remoteip: ip
+    }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-	const outcome = await result.json();
+  const outcome = await result.json();
 
   if (outcome.hostname !== 'kes.im') {
     return false;
   }
 
-	return outcome.success;
+  return outcome.success;
 }
