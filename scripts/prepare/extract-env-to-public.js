2062b6c1b516b5d7f0b7c7eafaaaab9b8088ffea
Rameş Aliyev <ramesaliyev@gmail.com>
2024-07-22 22:06:07 +0200

    refactor client code and fully integrate with shared verification, errors and env variables

---
+++ scripts/prepare/extract-env-to-public.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const toml = require('toml');
+
+const rootPath = path.resolve(__dirname, '../../');
+const wranglerTOMLFilePath = path.join(rootPath, 'wrangler.toml');
+const outputJSFileRelativePath = 'public/js/modules/env.js';
+const outputJSFilePath = path.join(rootPath, outputJSFileRelativePath);
+
+// Read the TOML file
+fs.readFile(wranglerTOMLFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error while reading the wrangler.toml:', err);
+    return;
+  }
+
+  try {
+    // Parse the TOML file
+    const config = toml.parse(data);
+
+    // Extract the vars section.
+    const vars = config.vars;
+
+    // Convert vars to a JS file content
+    const jsContent = `export const ENV = ${JSON.stringify(vars, null, 0)};\n`;
+    
+    // Create final content
+    const comment = 'This file is auto-generated from the "wrangler.toml" vars.';
+    const fileContent = `\n// ${comment}\n\n${jsContent}`;
+
+    // Write the JS content to the file
+    fs.writeFile(outputJSFilePath, fileContent, (err) => {
+      if (err) {
+        console.error('Error writing the env.js file:', err);
+        return;
+      }
+      
+      console.log(`[SUCCESS] ${outputJSFileRelativePath} file generated from wrangler.toml vars.`);
+    });
+  } catch (err) {
+    console.error('Error while parsing the wrangler.toml', err);
+  }
+});
--- scripts/prepare/index.js
+++ scripts/prepare/index.js
@@ -4,3 +4,5 @@ require('./generate-banned');
 // Compile some .ts files to .js and save them in the public/js/modules
 require('./compile-to-public');
 
+// Extract ENV variables from wrangler.toml and save them in public/js/modules/env.js
+require('./extract-env-to-public');
