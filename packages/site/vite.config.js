44f2a60519b3fa7c3becc002ca0ca7c0cbaa4b48
Rameş Aliyev <ramesaliyev@gmail.com>
2024-07-28 18:13:56 +0200

    Replace html with pug inside site

--- packages/site/vite.config.js
+++ packages/site/vite.config.js
@@ -1,8 +1,10 @@
+import fs from 'fs';
 import {resolve} from 'path'
 import {loadTsConfig} from "load-tsconfig"
 import {defineConfig, loadEnv} from 'vite';
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import pug from 'pug';
 
 const root = resolve(__dirname);
 
@@ -26,9 +28,50 @@ function generateResolveAlias() {
   return alias;
 }
 
+/**
+ * Trick vite/rollup to behave with pug.
+ * I just want to render some pug files for god's sake.
+ */
+// If true, rendered pug files won't be cached, so they'll re-render on each vite compilation.
+const PUG_DEV = false;
+// These fake HTML files are actually their corresponding Pug files.
+const inputHTMLNames = [
+  'index.html',
+  '404.html',
+  '500.html',
+]
+// Create fake html files
+inputHTMLNames.forEach(filename => {
+  fs.writeFileSync(resolve(root, filename), '');
+});
+// Cache compiled pug files.
+const getCompiledPug = (() => {
+  const cache = {};
+  return (filename) => {
+    if (PUG_DEV || !cache[filename]) {
+      const pugFilename = filename.replace('.html', '.pug');
+      const content = fs.readFileSync(pugFilename, 'utf-8');
+      cache[filename] = pug.render(content, {filename: pugFilename});
+    }
+
+    return cache[filename];
+  };
+})();
+// Compile plugin
+const compilePug = () => ({
+  name: 'transform-html',
+  transformIndexHtml: {
+    order: 'pre',
+    handler(content, {path, filename}) {
+      return getCompiledPug(filename);
+    }
+  }
+});
+
 export default defineConfig(({command, mode}) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  /**
+   * Define environment variables.
+   */
   const rawEnv = loadEnv(mode, process.cwd(), 'APP_');
   const env = Object.fromEntries(
     Object.entries(rawEnv).map(([key, value]) => {
@@ -59,18 +102,14 @@ export default defineConfig(({command, mode}) => {
         transformMixedEsModules: true,
       },
       rollupOptions: {
-        input: [
-          'index.html',
-          '404.html',
-          '500.html',
-        ].map((file) => resolve(root, file)),
+        input: inputHTMLNames.map((file) => resolve(root, file)),
         external: [
           'dist',
         ],
         output: {
           entryFileNames: `assets/[name].hashed.[hash].js`,
           chunkFileNames: `assets/[name].hashed.[hash].js`,
-          assetFileNames: `assets/[name].hashed.[hash].[ext]`
+          assetFileNames: `assets/[name].hashed.[hash].[ext]`,
         }
       },
     },
@@ -84,23 +123,11 @@ export default defineConfig(({command, mode}) => {
       checker({
         typescript: true,
       }),
+      compilePug(),
     ],
 
     define: {
       env,
     },
   }
-})
-
-// vite.config.js
-// import { defineConfig } from 'vite'
-
-// export default defineConfig({
-//   esbuild: {
-//     jsxFactory: 'h',
-//     jsxFragment: 'Fragment',
-//   },
-// })
-
-// https://vitejs.dev/config/shared-options.html#esbuild
-// https://stackoverflow.com/questions/41557309/typescript-jsx-without-react
+});
